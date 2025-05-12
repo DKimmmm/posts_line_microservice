02ed67d98444a66262815e9720519e5ed434fc29
KoDi586 <138789200+KoDi586@users.noreply.github.com>
2025-05-12 14:18:37 +0300

    init

---
+++ src/main/java/com/example/postsline/PostslineApplication.java
@@ -0,0 +1,13 @@
+package com.example.postsline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostslineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostslineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/postsline/PostslineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postsline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostslineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
